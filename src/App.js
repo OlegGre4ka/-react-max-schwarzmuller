67b45918b3ade391c31463aa35379cda5c6b0460
OlegGre4ka <gre4kae@ukr.net>
2019-05-09 20:11:33 +0300

    Passing Methods References+Two way Binding

--- src/App.js
+++ src/App.js
@@ -5,102 +5,122 @@ import Person from './Person/Person'
 
 // example stateful class-based component
 
-// class App extends React.Component {
-//   state = {
-//     persons: [
-//       { name: 'Oleg', age: 42 },
-//       { name: 'Max', age: 29 }
-//     ]
-//   }
-
-//   switchNameHandler = () => {
-//     console.log('Was swirched name!');
-//     this.setState({
-//       persons: [
-//         { name: 'Oleg', age: 42 },
-//         { name: 'Maximillian', age: 29 }
-//       ]
-//     })
-//   }
-
-//   render() {
-
-//     return (
-//       <div className="App">
-//         <header className="App-header">
-//           <img src={logo} className="App-logo" alt="logo" />
-//           <p>
-//             Hello React course from Max!
-//         </p>
-//           <button onClick={this.switchNameHandler}>Switch Name</button>
-//           <Person name={this.state.persons[0].name} age={this.state.persons[0].age} >My hobbies: Music</Person>
-//           <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
-  
-//           {/* <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a> */}
-//         </header>
-//       </div>
-//     );
-//   }
-
-// }
-
-// export default App;
-
-// example React Hooks - UseState() in functional component
-
-const App = () => {
-  const [personsState, setPresonsState] = useState({
+class App extends React.Component {
+  state = {
     persons: [
       { name: 'Oleg', age: 42 },
-      { name: 'Max', age: 29 }
-    ],
-    otherState: 'Other State'
-  });
+      { name: 'Max', age: 29 },
+      { name: 'Anna', age: 25 }
+    ]
+  }
 
-  const [otherState, setOtherState] = useState({
-    otherValue: 'First State'
-  })
-
-  console.log(personsState, 'personsState');
-  console.log(otherState, 'otherState');
-
-  const switchNameHandler = () => {
-    // console.log('Was swirched name!');
-    setPresonsState({
+  switchNameHandler = (newName) => {
+    console.log('Was swirched name!');
+    this.setState({
       persons: [
         { name: 'Oleg', age: 42 },
-        { name: 'Maximillian', age: 29 }
-      ],
-      otherState: 'Other Second State'
+        { name:newName, age: 29 },
+        { name: 'Anna', age: 25 }
 
-    })
-    setOtherState({
-      otherValue: 'Second State'
+      ]
     })
   }
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello React course from Max!
+  nameChanchedHandler = event => {
+    this.setState({
+      persons: [
+        { name: 'Oleg', age: 42 },
+        { name:event.target.value, age: 29 },
+        { name: 'Anna', age: 25 }
+
+      ]
+    })
+  }
+
+  render() {
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Hello React course from Max!
         </p>
+          <button onClick={this.switchNameHandler.bind(this,'Maria')}>Switch Name</button>
+          <Person name={this.state.persons[0].name} age={this.state.persons[0].age} >My hobbies: Music</Person>
+          <Person 
+          changed = {this.nameChanchedHandler}          
+          name={this.state.persons[1].name} age={this.state.persons[1].age} />
+          <Person 
+          click={()=>this.switchNameHandler('Mark')} 
+          // changed = {this.nameChanchedHandler}
+          name={this.state.persons[2].name} age={this.state.persons[2].age} />
 
-        <button onClick={switchNameHandler}>Switch Name</button>
+        </header>
+      </div>
+    );
+  }
 
-        <Person name={personsState.persons[0].name} age={personsState.persons[0].age} >My hobbies: Music</Person>
-        <Person name={personsState.persons[1].name} age={personsState.persons[1].age} />
-      </header>
-    </div>
-  );
 }
 
 export default App;
+
+// example React Hooks - UseState() in functional component
+
+// const App = () => {
+//   const [personsState, setPresonsState] = useState({
+//     persons: [
+//       { name: 'Oleg', age: 42 },
+//       { name: 'Max', age: 29 }
+//     ],
+//     otherState: 'Other State'
+//   });
+
+//   const [otherState, setOtherState] = useState({
+//     otherValue: 'First State'
+//   })
+
+//   console.log(personsState, 'personsState');
+//   console.log(otherState, 'otherState');
+
+//   const switchNameHandler = () => {
+//     // console.log('Was swirched name!');
+//     setPresonsState({
+//       persons: [
+//         { name: 'Oleg', age: 42 },
+//         { name: 'Maximillian', age: 29 }
+//       ],
+//       otherState: 'Other Second State'
+
+//     })
+//     setOtherState({
+//       otherValue: 'Second State'
+//     })
+//   }
+
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Hello React course from Max!
+//         </p>
+
+//         <button onClick={switchNameHandler}>Switch Name</button>
+
+//         <Person name={personsState.persons[0].name} age={personsState.persons[0].age} >My hobbies: Music</Person>
+//         <Person name={personsState.persons[1].name} age={personsState.persons[1].age} />
+        {/* <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a> */}
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -2,7 +2,10 @@ import React from 'react';
 
 const Person = (props) => {
     return (
-        <p>I'm a {props.name} and I'm {props.age} years old! <span>{props.children}</span></p>
+        <>
+        <p onClick={props.click}>I'm a {props.name} and I'm {props.age} years old! <span>{props.children}</span></p>
+        <input type="text" onChange={props.changed} value={props.name}/>
+        </>
     )
 }
 export default Person;
